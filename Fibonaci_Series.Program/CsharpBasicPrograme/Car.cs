243a28fcf06b939d373ed0dccc613e277b08214f
Rajajr <rajjr011@gmail.com>
2022-09-04 00:37:52 +0700

    SumOfDigit

---
+++ Fibonaci_Series.Program/CsharpBasicPrograme/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicPrograme
+{
+    public class Car
+    {
+        public string color;
+        public string price;
+        public string model;
+
+    public void CarDetails()
+        {
+            Console.WriteLine("Carcolour={0} Carprice={1} Carmodel={2}", color, price,model);
+        }
+    }
+}
---
+++ Fibonaci_Series.Program/CsharpBasicPrograme/FibonocciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicPrograme
+{
+    public class FibonocciSeries
+    {
+        public static void Findseries(int n)
+        {
+            int first = 0;
+            int second = 1;
+            int third = 0,res = 0;
+            Console.Write("{0} {1}",first,second);
+            third = first + second;
+            for (int i= 2; i < n; i++)
+            {
+                third = first + second;
+                Console.Write("{0}", third);
+                first = second;
+                second = third;
+                
+            }
+            
+
+            }
+}
